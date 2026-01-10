49edae3b1f7965bc7663ed1793cfe2aec5ab3381
chhot2 <chhot@tutanota.de>
2026-01-10 19:10:32 +0530

    Delete proxy-browser directory

--- proxy-browser/crates/browser-core/tests/fingerprint_tests.rs
+++
@@ -1,9 +0,0 @@
-//! Unit tests for the fingerprint module.
-
-use browser_core::*;
-
-#[test]
-fn test_browserfingerprint_basic() {
-    // Basic test for BrowserFingerprint
-    assert!(true, "BrowserFingerprint basic test placeholder");
-}
